dff8ffc88a05dca7dd52a72c70856bcede1629fe
argianardi <argianardi1435@gmail.com>
2022-06-28 22:03:15 +0700

    if statement with comparison

---
+++ week 3/day 3/controlFlow.js
@@ -0,0 +1,17 @@
+// If statement with comparison
+let myAge = 18;
+
+if (myAge === 5) {
+  console.log('You too young');
+}
+if (myAge === 17) {
+  console.log('So Sweet');
+}
+if (myAge > 17 && myAge <= 38) {
+  console.log('myAge is beetwen 17 and 30');
+}
+
+let dadAge = 9;
+if (dadAge < myAge) {
+  console.log(dadAge);
+}
