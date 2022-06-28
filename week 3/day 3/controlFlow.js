02a2ce6341585608892dbba84123976db4ab429f
argianardi <argianardi1435@gmail.com>
2022-06-29 06:32:16 +0700

    if, else if, else dan nested if statement

--- week 3/day 3/controlFlow.js
+++ week 3/day 3/controlFlow.js
@@ -23,3 +23,22 @@ if (number % 2 === 0) {
 } else {
   console.log('Ganjil');
 }
+
+// if, else if, else
+let hour = 11;
+if (hour < 12) {
+  console.log('selamat pagi');
+} else if (hour < 18) {
+  console.log('selamat sore');
+} else {
+  console.log('selamat malam');
+}
+
+// nested if statement
+let v1 = 400;
+let v2 = 700;
+if (v1 === 400) {
+  if (v2 === 700) {
+    console.log('value of v1 is 400 and v2 is 700');
+  }
+}
